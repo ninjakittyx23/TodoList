5331ac6bd9f384854a28f45dab25222aba6c2016
robert allen <bobbidigi11@gmail.com>
2018-11-14 11:53:24 -0500

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+//Check to-do's
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+//Click on X's to delete todo line
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event) {
+    if (event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li> <span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>")
+    }
+});
